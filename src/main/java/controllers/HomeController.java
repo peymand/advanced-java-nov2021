a907f61be422b16dc273c2e5ff95b77393879542
Peyman Dodangeh <Peyman@Dodangeh.com>
2021-11-23 15:28:16 +0330

    adding log framework

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -1,5 +1,9 @@
 package controllers;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,8 +13,19 @@ import java.io.IOException;
 
 @WebServlet("/home.do")
 public class HomeController extends HttpServlet {
+
+
+    private static final Logger logger = LogManager.getLogger(HomeController.class);
+
+
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        BasicConfigurator.configure();
+        logger.info("Hello world");
+        logger.info("we are in logger info mode");
+
         req.getRequestDispatcher("/WEB-INF/index.jsp").forward(req,resp);
     }
 }
